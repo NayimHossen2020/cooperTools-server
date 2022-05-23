63e56bb8543630199d3444d4d0049405f0e00e4b
Nayim hossen <nayimh2015@gmail.com>
2022-05-23 22:53:58 +0600

    api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,29 +9,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oumcp.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-  const productsCollection = client.db("toolsManufacturer").collection("products");
+async function run() {
+  try {
+    await client.connect();
+    const productsCollection = client.db("toolsManufacturer").collection("products");
 
-// async function run() {
-//   try {
-//     await client.connect();
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = productsCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
 
-//   }
-//   finally {
+  } finally {
+  }
+}
 
-//   }
-// }
+run().catch(console.dir);
 
-// run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-  res.send('From tools manufacture')
-})
+app.get("/", (req, res) => {
+  res.send("From tools manufacture");
+});
 
 app.listen(port, () => {
-  console.log(`tools manufacture App listening on port ${port}`)
-})
+  console.log(`tools manufacture App listening on port ${port}`);
+});
