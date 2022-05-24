c378f100fefc7436e8b11bbd5a919c4df8b2900e
Nayim hossen <nayimh2015@gmail.com>
2022-05-24 22:01:13 +0600

    admin identify using email

--- index.js
+++ index.js
@@ -56,6 +56,39 @@ async function run() {
       res.send(product);
     });
 
+    //load all users
+    app.get('/user', verifyJWT, async (req, res) => {
+      const users = await usersCollection.find().toArray();
+      res.send(users);
+    })
+
+
+    //checking admin or not
+    app.get('/admin/:email', async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const user = await usersCollection.findOne({ email: email });
+      const isAdmin = user.role === 'admin';
+      res.send({ isAdmin: isAdmin })
+    })
+
+    //set admin in 
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await usersCollection.findOne({ email: requester });
+      if (requesterAccount.role === 'admin') {
+        const filter = { email: email };
+        const updateDoc = {
+          $set: { role: 'admin' },
+        };
+        const result = await usersCollection.updateOne(filter, updateDoc);
+        res.send(result)
+      }
+      else {
+        res.status(403).send({ message: 'forbidden' })
+      }
+    })
 
     //user add and update
     app.put('/user/:email', async (req, res) => {
